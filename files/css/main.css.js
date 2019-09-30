b79a93b0bbc0f57be12976a9941db480a5e09160
Paul Frazee <pfrazee@gmail.com>
2019-09-30 10:02:26 -0500

    Add files viewer app

---
+++ files/css/main.css.js
@@ -0,0 +1,35 @@
+import {css} from '../../vendor/beaker-app-stdlib/vendor/lit-element/lit-element.js'
+import buttonsCSS from '../../vendor/beaker-app-stdlib/css/buttons2.css.js'
+import inputsCSS from '../../vendor/beaker-app-stdlib/css/inputs.css.js'
+import tooltipCSS from '../../vendor/beaker-app-stdlib/css/tooltip.css.js'
+
+const cssStr = css`
+${buttonsCSS}
+${inputsCSS}
+${tooltipCSS}
+
+:host {
+  display: grid;
+  grid-template-columns: 1fr 400px;
+  grid-gap: 30px;
+  height: 100%;
+  padding: 10px;
+}
+
+file-display {
+  display: block;
+  border: 1px solid #e3e3ee;
+  border-radius: 8px;
+  padding: 10px;
+}
+
+@media (max-width: 800px) {
+  :host {
+    grid-template-columns: 1fr;
+  }
+  file-info {
+    display: none;
+  }
+}
+`
+export default cssStr
